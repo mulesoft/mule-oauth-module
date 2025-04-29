d003ce3ca41d88c7e9c42b173f872a7e85709659
lvarri <lvarri@salesforce.com>
2025-04-29 15:17:52 +0530

    Adding more tests DefaultAuthorizationCodeGrantType

--- src/test/java/org/mule/test/oauth2/api/authorizationcode/DefaultAuthorizationCodeGrantTypeTest.java
+++ src/test/java/org/mule/test/oauth2/api/authorizationcode/DefaultAuthorizationCodeGrantTypeTest.java
@@ -141,6 +141,34 @@ public class DefaultAuthorizationCodeGrantTypeTest {
     grantType1.setExternalCallbackUrl("http://example.com/callback");
     grantType2.setExternalCallbackUrl("http://example.com/callback");
 
+    grantType1.setLocalCallbackConfig("config1");
+    grantType2.setLocalCallbackConfig("config1");
+
+    grantType1.setLocalCallbackConfigPath("path1");
+    grantType2.setLocalCallbackConfigPath("path1");
+
+    grantType1.setLocalAuthorizationUrl("authUrl1");
+    grantType2.setLocalAuthorizationUrl("authUrl1");
+
+    grantType1.setAuthorizationUrl("authUrl1");
+    grantType2.setAuthorizationUrl("authUrl1");
+
+    Literal<String> state1 = mock(Literal.class);
+    grantType1.setState(state1);
+    grantType2.setState(state1);
+
+    Map<String, String> customParameters1 = new HashMap<>();
+    customParameters1.put("key1", "value1");
+    grantType1.getCustomParameters().putAll(customParameters1);
+    grantType2.getCustomParameters().putAll(customParameters1);
+
+    ParameterResolver<String> resourceOwnerId1 = mock(ParameterResolver.class);
+    grantType1.setResourceOwnerId(resourceOwnerId1);
+    grantType2.setResourceOwnerId(resourceOwnerId1);
+
+    grantType1.setEncodeClientCredentialsInBody(true);
+    grantType2.setEncodeClientCredentialsInBody(true);
+
     assertTrue(grantType1.equals(grantType2));
     assertEquals(grantType1.hashCode(), grantType2.hashCode());
 
@@ -148,6 +176,57 @@ public class DefaultAuthorizationCodeGrantTypeTest {
 
     assertFalse(grantType1.equals(grantType2));
     assertNotEquals(grantType1.hashCode(), grantType2.hashCode());
+
+    assertFalse(grantType1.equals(new Object()));
+  }
+
+  @Test
+  public void testNotEqualsAndHashCode() {
+    DefaultAuthorizationCodeGrantType grantType1 = new DefaultAuthorizationCodeGrantType();
+    DefaultAuthorizationCodeGrantType grantType2 = new DefaultAuthorizationCodeGrantType();
+
+    grantType1.setLocalCallbackUrl("http://localhost:8080/callback");
+    grantType2.setLocalCallbackUrl("http://localhost:8080/callback2");
+
+    grantType1.setExternalCallbackUrl("http://example.com/callback");
+    grantType2.setExternalCallbackUrl("http://example.com/callback2");
+
+    grantType1.setLocalCallbackConfig("config1");
+    grantType2.setLocalCallbackConfig("config2");
+
+    grantType1.setLocalCallbackConfigPath("path1");
+    grantType2.setLocalCallbackConfigPath("path2");
+
+    grantType1.setLocalAuthorizationUrl("authUrl1");
+    grantType2.setLocalAuthorizationUrl("authUrl2");
+
+    grantType1.setAuthorizationUrl("authUrl1");
+    grantType2.setAuthorizationUrl("authUrl2");
+
+    Literal<String> state1 = mock(Literal.class);
+    Literal<String> state2 = mock(Literal.class);
+    grantType1.setState(state1);
+    grantType2.setState(state2);
+
+    Map<String, String> customParameters1 = new HashMap<>();
+    customParameters1.put("key1", "value1");
+    Map<String, String> customParameters2 = new HashMap<>();
+    customParameters2.put("key2", "value2");
+    grantType1.getCustomParameters().putAll(customParameters1);
+    grantType2.getCustomParameters().putAll(customParameters2);
+
+    ParameterResolver<String> resourceOwnerId1 = mock(ParameterResolver.class);
+    ParameterResolver<String> resourceOwnerId2 = mock(ParameterResolver.class);
+    grantType1.setResourceOwnerId(resourceOwnerId1);
+    grantType2.setResourceOwnerId(resourceOwnerId2);
+
+    grantType1.setEncodeClientCredentialsInBody(true);
+    grantType2.setEncodeClientCredentialsInBody(false);
+
+    assertFalse(grantType1.equals(grantType2));
+    assertNotEquals(grantType1.hashCode(), grantType2.hashCode());
+
+    assertFalse(grantType1.equals(new Object()));
   }
 
   @Test
